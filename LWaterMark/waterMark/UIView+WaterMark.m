56460f4b6a8c107a168241f55460552fe3932a35
lang <langqingbao@sina.com>
2021-11-11 16:20:04 +0800

    修改添加位置

--- LWaterMark/waterMark/UIView+WaterMark.m
+++ LWaterMark/waterMark/UIView+WaterMark.m
@@ -9,59 +9,63 @@
 #import <objc/runtime.h>
 
 @interface UIView ()
-@property (nonatomic,  strong) NSMutableArray * listLayer;
+@property (nonatomic,  strong) CALayer * bgLayer;
 @end
 
 @implementation UIView (WaterMark)
-static char kAssociatedObjectKey_listLayer;
 
--(void)setListLayer:(NSMutableArray *)listLayer
-{
-    objc_setAssociatedObject(self, &kAssociatedObjectKey_listLayer, listLayer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+static char kAssociatedObjectKey_bgLayer;
+
+- (void)setBgLayer:(CALayer *)bgLayer {
+    objc_setAssociatedObject(self, &kAssociatedObjectKey_bgLayer, bgLayer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
--(NSMutableArray *)listLayer
-{
-    return objc_getAssociatedObject(self, &kAssociatedObjectKey_listLayer);
+- (CALayer *)bgLayer {
+    return objc_getAssociatedObject(self, &kAssociatedObjectKey_bgLayer);
 }
 
 - (void)removeTextLayers{
-    NSArray *arr =  [NSArray arrayWithArray:self.listLayer];
-    for (CATextLayer *layer in arr) {
-        [layer removeFromSuperlayer];
-    }
+    [self.bgLayer removeFromSuperlayer];
+    self.bgLayer = nil;
 }
 
 - (void)addWaterMarkWithText:(NSString*)waterText textColor:(UIColor*)color font:(UIFont*)font {
-    self.listLayer = NSMutableArray.array;
-    //计算水印文字的宽高
-    NSString *waterMark = waterText;
-    CGSize textSize = [waterMark sizeWithAttributes:@{NSFontAttributeName:font,NSForegroundColorAttributeName:color}];
-    CGFloat height =  [UIScreen mainScreen].bounds.size.height;
-    CGFloat width =  [UIScreen mainScreen].bounds.size.width;
-    NSInteger line = height*3.5/80; //多少行
-    NSInteger row = 20;
-    
-    for (int i = 0; i < line; i ++) {
-        for (int j = 0; j < row; j ++) {
-            
-            CATextLayer *textLayer = [[CATextLayer alloc]init];
-            textLayer.contentsScale = [UIScreen mainScreen].scale;//按当前屏幕分辨显示，否则会模糊
-            CFStringRef fontName = (__bridge CFStringRef)font.fontName;
-            CGFontRef fontRef =CGFontCreateWithFontName(fontName);
-            textLayer.font = fontRef;
-            textLayer.fontSize = font.pointSize;
-            textLayer.foregroundColor = color.CGColor;
-            textLayer.string = waterMark;
-            textLayer.frame = CGRectMake(j*(textSize.width+30),  i*60, textSize.width, textSize.height+ 2);
-            //旋转文字
-            textLayer.transform = CATransform3DMakeRotation(-M_PI/5, 0,0,3);
-            [self.layer addSublayer:textLayer];
-            [self.listLayer addObject:textLayer];
+    @synchronized (self) {
+        
+        //计算水印文字的宽高
+        NSString *waterMark = waterText;
+        CGSize textSize = [waterMark sizeWithAttributes:@{NSFontAttributeName:font,NSForegroundColorAttributeName:color}];
+        CGFloat height =  [UIScreen mainScreen].bounds.size.height;
+        NSInteger line = height*3.5/80; //多少行
+        NSInteger row = 20;
+        [self removeTextLayers];
+
+        if (!self.bgLayer) {
+            self.bgLayer = CALayer.new;
+            self.bgLayer.backgroundColor = UIColor.clearColor.CGColor;
+            [self.layer addSublayer: self.bgLayer];
+        }
+        @autoreleasepool {
+            for (int i = 0; i < line; i ++) {
+                for (int j = 0; j < row; j ++) {
+                    
+                    CATextLayer *textLayer = [[CATextLayer alloc]init];
+                    textLayer.contentsScale = [UIScreen mainScreen].scale;//按当前屏幕分辨显示，否则会模糊
+                    CFStringRef fontName = (__bridge CFStringRef)font.fontName;
+                    CGFontRef fontRef =CGFontCreateWithFontName(fontName);
+                    textLayer.font = fontRef;
+                    textLayer.fontSize = font.pointSize;
+                    textLayer.foregroundColor = color.CGColor;
+                    textLayer.string = waterMark;
+                    textLayer.frame = CGRectMake(j*(textSize.width+30),  i*60, textSize.width, textSize.height+ 2);
+                    //旋转文字
+                    textLayer.transform = CATransform3DMakeRotation(-M_PI/5, 0,0,3);
+                    [self.bgLayer addSublayer:textLayer];
+                }
+            }
+
         }
     }
-    
-    NSArray * list = self.listLayer;
-    NSLog(@"");
+ 
 }
 
 @end
